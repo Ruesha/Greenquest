0612c38f539bfaed25cc03271f57e9e67845ed24
ruesha <princeadewale2005@gmail.com>
2026-03-03 17:49:56 +0100

    hideshopagain

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import CustomCursor from "./components/CustomCusor";
 import Landing from "./pages/Landing";
-import Shop from "./pages/Shop";
-import Checkout from "./components/Checkout";
-import PaymentSuccess from "./components/Paymentsuccess";
+
 
 
 const App = () => {
@@ -14,10 +12,7 @@ const App = () => {
       <CustomCursor />
       <Routes>
         <Route path="/" element={<Landing />} />
-        {/* <Route path="/shop" element={<Shop />} />
-        <Route path="/checkout" element={<Checkout />} />
-        <Route path="/payment-success" element={<PaymentSuccess />} />
-       */}
+        
       </Routes>
     </Router>
   );
